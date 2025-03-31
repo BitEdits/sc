8822c8fb03afde9d444638f414bf8ab0e558a248
5HT <maxim@synrc.com>
2025-03-31 16:17:59 +0300

    move clear bar upper

--- input.c
+++ input.c
@@ -12,6 +12,11 @@ void execute_command(const char *cmd) {
     char *command_copy = malloc(1024); //strdup(cmd);
     char *output_copy = malloc(65536); //strdup(output);
 
+    printf("\x1b[K");                 // Clear line
+    printf("\x1b[%d;1H", rows);       // Move to last row (rows is from get_window_size), clear line
+    printf("\x1b[%d;1H\x1b[1;37;40m%-*s", rows - 1, cols, "");
+    printf("\x1b[%d;1H", rows - 1);   // Move to last row (rows is from get_window_size), clear line
+
     // Create pipe to capture output
     if (pipe(pipefd) == -1) {
         snprintf(output, sizeof(output), "Error: Failed to create pipe");
@@ -66,10 +71,6 @@ void execute_command(const char *cmd) {
 
     // Suspend our raw mode to let child take over terminal
     disable_raw_mode();
-    printf("\x1b[K");                 // Clear line
-    printf("\x1b[%d;1H", rows);       // Move to last row (rows is from get_window_size), clear line
-    printf("\x1b[%d;1H\x1b[1;37;40m%-*s", rows - 1, cols, "");
-    printf("\x1b[%d;1H", rows - 1);   // Move to last row (rows is from get_window_size), clear line
     fflush(stdout);                   // Ensure sequences are applied
 
     // Read output in real-time and display it
